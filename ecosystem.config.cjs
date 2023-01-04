322a913f5c5c73d29658d62985f13109b582a5be
Owner <owner@Owners-MacBook-Pro-2.local>
2023-01-04 23:48:48 +0500

    feat: added dev server;

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -8,5 +8,16 @@ module.exports = {
         NODE_ENV: "production",
       },
     },
+    {
+      name: "needs-socket-dev",
+      script: "./src/index.js",
+      instances: 1,
+      exec_mode: "cluster",
+      restart_delay: 10000,
+      listen_timeout: 10000,
+      env: {
+        NODE_ENV: "development",
+      }
+    },
   ],
 };
